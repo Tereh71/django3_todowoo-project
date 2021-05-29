0a4ee0143c863ee40c80786a03e0b97f25eeed81
Tereh71 <lositsky_v@palmira.ck.ua>
2021-05-29 12:18:39 +0300

    Added models Todo

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Todo(models.Model):
+    #заголовок запису
+    title = models.CharField(max_length=100)
+    #запис
+    memo = models.TextField(blank=True)
+    #дата виконання
+    created = models.DateTimeField(auto_now_add=True)
+    #коли зроблено запис
+    datecomleted = models.DateTimeField(null=True, blank=True)
+    #важливість запису
+    important = models.BooleanField(default=False)
+    #прив'язка запису до користувача
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
