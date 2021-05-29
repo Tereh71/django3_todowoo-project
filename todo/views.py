6e33f5b2a9b0922416ec596415c8279d8d7ccc29
Tereh71 <lositsky_v@palmira.ck.ua>
2021-05-29 11:42:03 +0300

    Ended procedure authentitication users

--- todo/views.py
+++ todo/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render, redirect
 #імпортуємо модуль створення форми авторизації користувачів
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.db import IntegrityError
-from django.contrib.auth import login
+from django.contrib.auth import login, logout, authenticate
+
+
+def  home(request):
+    return render (request, 'todo/home.html')
 
 def signupuser(request):
     if request.method == 'GET':
@@ -21,6 +25,24 @@ def signupuser(request):
             print('Привіт')
             #вивести повідомлення про неспівпадання паролів
             return render(request, 'todo/signupuser.html',  {'form':UserCreationForm(), 'error':'Паролі не співпадають'})
+
+
+def loginuser(request):
+    if request.method == 'GET':
+        return render(request, 'todo/loginuser.html',  {'form':AuthenticationForm()})
+    else:
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'todo/loginuser.html',  {'form':AuthenticationForm(), 'error':'Пару логін/пароль не знайдено'})
+        else:
+            login(request, user)
+            return redirect('currenttodos')
+
+
+def logoutuser(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('home')
 
 def currenttodos(request):
     return render(request, 'todo/currenttodos.html')
